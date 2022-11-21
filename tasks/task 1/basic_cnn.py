2ad1475cb6c7c13a8d702223b74eea64b4bb6fa0
benaa1995 <ben.alaluf2@gmail.com>
2022-11-21 16:48:01 +0200

    update

--- tasks/task 1/basic_cnn.py
+++ tasks/task 1/basic_cnn.py
@@ -1,186 +1,159 @@
-                  
-                  #MNIST Handwritten Digit Recognition in PyTorch 
+#basic net from- https://medium.com/@nutanbhogendrasharma/pytorch-convolutional-neural-network-with-mnist-dataset-4e8a4265e118
 
-                  #Setting up the Environment
 
-#We will be using PyTorch to train a convolutional neural network to recognize MNIST's                   
 import torch
-import torchvision
-
-import matplotlib.pyplot as plt
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
+from torch import optim
+from torch.autograd import Variable
 
-                   #Preparing the Dataset
-
-n_epochs = 3
-#We'll use a batch_size of 64 for training
-batch_size_train = 64
-#size 1000 for testing on this dataset
-batch_size_test = 1000
-#learning_rate and momentum are hyperparameters for the optimizer we'll be using later on.
-learning_rate = 0.01
-momentum = 0.5
-log_interval = 10
-
-random_seed = 1
-torch.backends.cudnn.enabled = False
-torch.manual_seed(random_seed)
-
-#
-train_loader = torch.utils.data.DataLoader(
-  torchvision.datasets.MNIST('/files/', train=True, download=True,
-                             transform=torchvision.transforms.Compose([
-                               torchvision.transforms.ToTensor(),
-                               torchvision.transforms.Normalize(
-                                 (0.1307,), (0.3081,))
-                             ])),
-  batch_size=batch_size_train, shuffle=True)
-
-test_loader = torch.utils.data.DataLoader(
-  torchvision.datasets.MNIST('/files/', train=False, download=True,
-                             transform=torchvision.transforms.Compose([
-                               torchvision.transforms.ToTensor(),
-                               torchvision.transforms.Normalize(
-                                 (0.1307,), (0.3081,))
-                             ])),
-  batch_size=batch_size_test, shuffle=True)
-
-  #
-examples = enumerate(test_loader)
-batch_idx, (example_data, example_targets) = next(examples)
-
-#
-example_data.shape
-torch.Size([1000, 1, 28, 28])
+#constants
+BATCH_SIZE = 100
 
 
 
-#                                 לבחור רשת!!!!!!!!##$#%$#%@%$##
-fig = plt.figure()
-for i in range(6):
-  plt.subplot(2,3,i+1)
-  plt.tight_layout()
-  plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
-  plt.title("Ground Truth: {}".format(example_targets[i]))
-  plt.xticks([])
-  plt.yticks([])
-fig
 
-#                             
-class Net(nn.Module):
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+train_data = datasets.MNIST(
+    root = 'data',
+    train = True,                         
+    transform = ToTensor(), 
+    download = True,            
+)
+
+test_data = datasets.MNIST(
+    root = 'data', 
+    train = False, 
+    transform = ToTensor()
+)
+#prepar the data to train/ test by shuffle, load in batch instead of one by one etc.
+loaders = {
+    'train' : torch.utils.data.DataLoader(train_data, 
+                                          batch_size=100, 
+                                          shuffle=True, 
+                                          num_workers=0),
+    
+    'test'  : torch.utils.data.DataLoader(test_data, 
+                                          batch_size=100, 
+                                          shuffle=True, 
+                                          num_workers=0),
+}
+loaders
+
+
+# creat the convunetional neural network
+class CNN(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        #the base class get the "son" name and the module
+        super(CNN, self).__init__()
+        #define the first convolution
+        # img size= (28*28)->1 padding ->(30*30)->conv kernal 3->(28*28)->
+        # max pooling kernal 2->(14*14)
+        #Cin=1, Cout=32
+        self.conv1 = nn.Sequential(         
+            #add convolution
+            nn.Conv2d(
+                in_channels=1,              
+                out_channels=32,            
+                kernel_size=3,              
+                stride=1,                   
+                padding=1,                  
+            ),              
+            #wrap the convolution with relu                
+            nn.ReLU(), 
 
+            nn.MaxPool2d(kernel_size = 2, stride = 2),                        
+        )
+        
+        #define the second convolution
+        # img size= (14*14)->1 padding ->(16*16)->conv kernal 3->(14*14)->
+        # max pooling kernal 2->(7*7)
+        #Cin=32, Cout=64
+        self.conv2 = nn.Sequential(         
+            nn.Conv2d(32, 64, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2),
+        )
+
+        self.relu = nn.ReLU()
+        # fully connected layer
+        #img size (7*7)-> flatten the img->(3134*1)
+        #  Cin 64 , output (128*1) classes
+        self.linear1 = nn.Linear(64 * 7 * 7, 128)
+        #  in (128*1) , output (10*1) classes
+        self.out = nn.Linear(128, 10)
+    #combine all the "blocks" 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x)
-#
-network = Net()
-optimizer = optim.SGD(network.parameters(), lr=learning_rate,
-                      momentum=momentum)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        # flatten the output of conv2 to (batch_size, 32 * 7 * 7=1568)
+        x = x.view(x.size(0), -1) 
+        x=self.linear1(x)
+        x=self.relu(x)
+        output = self.out(x)
+        return output, x    # return x for visualization
+cnn = CNN()
+#print( cnn )
 
-#
-train_losses = []
-train_counter = []
-test_losses = []
-test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
-#
-def train(epoch):
-  network.train()
-  for batch_idx, (data, target) in enumerate(train_loader):
-    optimizer.zero_grad()
-    output = network(data)
-    loss = F.nll_loss(output, target)
-    loss.backward()
-    optimizer.step()
-    if batch_idx % log_interval == 0:
-      print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-        epoch, batch_idx * len(data), len(train_loader.dataset),
-        100. * batch_idx / len(train_loader), loss.item()))
-      train_losses.append(loss.item())
-      train_counter.append(
-        (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
-      torch.save(network.state_dict(), '/results/model.pth')
-      torch.save(optimizer.state_dict(), '/results/optimizer.pth')
-#
+#define the "cross entropy" loss function
+loss_func = nn.CrossEntropyLoss()
+#print(loss_func)
+
+# take the betas from our CNN class
+optimizer = optim.Adam(cnn.parameters(), lr = 0.01)
+#print(optimizer)
+
+num_epochs = 10
+def train(num_epochs, cnn, loaders):
+    
+    cnn.train()
+        
+    # Train the model
+    total_step = len(loaders['train'])
+        
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(loaders['train']):
+            
+            # gives batch data, normalize x when iterate train_loader
+            b_x = Variable(images)   # batch x
+            b_y = Variable(labels)   # batch y
+            output = cnn(b_x)[0]               
+            loss = loss_func(output, b_y)
+            
+            # clear gradients for this training step   
+            optimizer.zero_grad()           
+            
+            # backpropagation, compute gradients 
+            loss.backward()    
+            # apply gradients             
+            optimizer.step()                
+            
+            if (i+1) % 100 == 0:
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                       .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+            pass
+        
+        pass  
+    
+    pass
+train(num_epochs, cnn, loaders)
+
 def test():
-  network.eval()
-  test_loss = 0
-  correct = 0
-  with torch.no_grad():
-    for data, target in test_loader:
-      output = network(data)
-      test_loss += F.nll_loss(output, target, size_average=False).item()
-      pred = output.data.max(1, keepdim=True)[1]
-      correct += pred.eq(target.data.view_as(pred)).sum()
-  test_loss /= len(test_loader.dataset)
-  test_losses.append(test_loss)
-  print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-    test_loss, correct, len(test_loader.dataset),
-    100. * correct / len(test_loader.dataset)))
-#
+    # Call the base class to go to test the model
+    cnn.eval()
 
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in loaders['test']:
+            test_output, last_layer = cnn(images)
+            pred_y = torch.max(test_output, 1)[1].data.squeeze()
+            accuracy = (pred_y == labels).sum().item() / float(labels.size(0))
+            pass
+        print('Test Accuracy of the model on the 10000 test images: %.2f' % accuracy)
+    
+pass
 test()
-for epoch in range(1, n_epochs + 1):
-  train(epoch)
-  test()
-
-#
-fig = plt.figure()
-plt.plot(train_counter, train_losses, color='blue')
-plt.scatter(test_counter, test_losses, color='red')
-plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
-plt.xlabel('number of training examples seen')
-plt.ylabel('negative log likelihood loss')
-fig
-
-#
-with torch.no_grad():
-  output = network(example_data)
-#
-fig = plt.figure()
-for i in range(6):
-  plt.subplot(2,3,i+1)
-  plt.tight_layout()
-  plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
-  plt.title("Prediction: {}".format(
-    output.data.max(1, keepdim=True)[1][i].item()))
-  plt.xticks([])
-  plt.yticks([])
-fig
-#
-
-continued_network = Net()
-continued_optimizer = optim.SGD(network.parameters(), lr=learning_rate,
-#
-network_state_dict = torch.load()
-continued_network.load_state_dict(network_state_dict)
-
-optimizer_state_dict = torch.load()
-continued_optimizer.load_state_dict(optimizer_state_dict)
-#
-for i in range(4,9):
-  test_counter.append(i*len(train_loader.dataset))
-  train(i)
-  test()
-#
-fig = plt.figure()
-plt.plot(train_counter, train_losses, color='blue')
-plt.scatter(test_counter, test_losses, color='red')
-plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
-plt.xlabel('number of training examples seen')
-plt.ylabel('negative log likelihood loss')
-fig
-
