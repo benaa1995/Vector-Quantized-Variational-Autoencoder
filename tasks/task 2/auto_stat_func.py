79e1474a33a0e5efb0f9260b4d72ff5df8daf872
benaa1995 <ben.alaluf2@gmail.com>
2022-12-29 23:41:55 +0200

    convert image from latent

--- tasks/task 2/auto_stat_func.py
+++ tasks/task 2/auto_stat_func.py
@@ -60,11 +60,11 @@ def create_random_img(decoder, num_of_image=10, latent_size=4):
 # every digit in the vector on the image
 
 """
-    test_function does change every digit from latent vector in range of [-1,1] to check the image influetion from encoder.
+    latent_digit_impact does change every digit from latent vector in range of [-1,1] to check the image influetion from encoder.
 
     :param encoder: the encoder after the train proccess 
     :param decoder: the decoder after the train proccess 
-    :param decoder: the test dataset 
+    :param test_dataset: the test dataset 
     :param num_of_image: the number of image to create
     :param latent_size: the latent size of the autoencoder 
     :param num_of_steps: the number of steps of every  digit in range of [-1,1]
@@ -72,7 +72,6 @@ def create_random_img(decoder, num_of_image=10, latent_size=4):
     :return: none
     """ 
 def latent_digit_impact(encoder, decoder,test_dataset, num_of_image=8, latent_size=4, num_of_steps=8,
-
                         device=torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")):
     origin_image_list = []
     image_vec_list = []
@@ -122,3 +121,62 @@ def latent_digit_impact(encoder, decoder,test_dataset, num_of_image=8, latent_si
                     # add to the the converted image to the changed list
                     changed_image_list.append(changed_image)
             plt.show()
+
+
+ # The function two image and convert one imag to the second by margin the two latent vector
+ # with different impact
+"""
+    convert_img_from_latent 
+    :param encoder: the encoder after the train proccess 
+    :param decoder: the decoder after the train proccess 
+    :param test_dataset: the test dataset
+    :param num_of_image: the number of image to create
+    :param latent_size: the latent size of the autoencoder 
+    :param num_of_steps: the number of steps of every  digit in range of [-1,1]
+    :param device: the torch device 
+    :return: none
+    """ 
+def convert_img_from_latent(encoder, decoder,test_dataset,num_of_image=10, latent_size=4, num_of_steps=8,
+                            device=torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")):
+    origin_image_list = []
+    image_vec_list = []
+    print("convert_img_from_latent device = ", device)
+    encoder.eval()
+    decoder.eval()
+    # plo image size
+    plt.figure(figsize=(16, 4.5))
+    # get the image
+    targets = test_dataset.targets.numpy()
+    t_idx = {i: np.where(targets == i%10)[0][0] for i in range(num_of_image)}
+    with torch.no_grad():
+        # save the requested original image and the latent vector
+        for i in range(num_of_image):
+            origin_image_list.append(test_dataset[t_idx[i]][0].unsqueeze(0).to(device))
+            image_vec_list.append(encoder(origin_image_list[i]))
+        # create the converted image
+        for img_index in range(len(image_vec_list) // 2):
+            temp_first_vec = image_vec_list[img_index].detach().clone()
+            temp_second_vec = image_vec_list[-img_index - 1].detach().clone()
+            # ax = plt.subplot(latent_size, n + 1, 12)
+            # plt.imshow(origin_image_list[img_index].cpu().squeeze().numpy(), cmap='gist_gray')
+            # ax.get_xaxis().set_visible(False)
+            # ax.get_yaxis().set_visible(False)
+            #
+            # ax = plt.subplot(latent_size, n + 1, 13)
+            # plt.imshow(origin_image_list[-img_index - 1].cpu().squeeze().numpy(), cmap='gist_gray')
+            # ax.get_xaxis().set_visible(False)
+            # ax.get_yaxis().set_visible(False)
+
+            for step in range(num_of_steps + 1):
+                t = (1 / num_of_steps) * step
+                # combine the two image with impact "t" on the first ant "t-1" on the second
+                temp_vec = temp_first_vec * (1 - t) + temp_second_vec * (t)
+
+                # creat the change image by pass the vector throw the decoder
+                changed_image = decoder(temp_vec)
+                # add to the converted image to the plot
+                ax = plt.subplot(1, num_of_image + 1, step + 1)
+                plt.imshow(changed_image.cpu().squeeze().numpy(), cmap='gist_gray')
+                ax.get_xaxis().set_visible(False)
+                ax.get_yaxis().set_visible(False)
+            plt.show()           
