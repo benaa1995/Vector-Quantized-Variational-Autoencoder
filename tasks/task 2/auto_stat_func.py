115c2ca142b725320b9800d3b355cdd48b73c5b2
benaa1995 <ben.alaluf2@gmail.com>
2022-12-29 23:19:07 +0200

    latent digit impact

--- tasks/task 2/auto_stat_func.py
+++ tasks/task 2/auto_stat_func.py
@@ -35,8 +35,8 @@ from sklearn.linear_model import LogisticRegression
 
     :param decoder: the decoder after the train proccess 
     :param num_of_image: the number of image to create
-    :param latent_size: the latent size of the autoencoder net
-
+    :param latent_size: the latent size of the autoencoder 
+    :return: none
     """ 
 def create_random_img(decoder, num_of_image=10, latent_size=4):
     # Set evaluation mode for the decoder
@@ -56,3 +56,63 @@ def create_random_img(decoder, num_of_image=10, latent_size=4):
             ax.get_yaxis().set_visible(False)
         plt.show()
 
+# The function take n image and change every digit in ther latent vector to chack what the impcat of
+# every digit in the vector on the image
+
+"""
+    test_function does change every digit from latent vector in range of [-1,1] to check the image influetion from encoder.
+
+    :param encoder: the decoder after the train proccess 
+    :param decoder: describe about parameter p2
+    :param num_of_image: the number of image to create
+    :param latent_size: the latent size of the autoencoder 
+    :param num_of_steps: the number of steps of every  digit in range of [-1,1]
+    :param device: the torch device 
+    :return: none
+    """ 
+def latent_digit_impact(encoder, decoder, num_of_image=8, latent_size=4, num_of_steps=8,
+                        device=torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")):
+    origin_image_list = []
+    image_vec_list = []
+    changed_image_list = []
+    encoder.eval()
+    decoder.eval()
+    # plo image size
+    plt.figure(figsize=(16, 4.5))
+    # get the image
+    targets = test_dataset.targets.numpy()
+    t_idx = {i: np.where(targets == i%10)[0][0] for i in range(num_of_image)}
+
+    with torch.no_grad():
+        # save the original image and the latent vector
+        for i in range(num_of_image):
+            # ax = plt.subplot(latent_size,n+3,i+1)
+            origin_image_list.append(test_dataset[t_idx[i]][0].unsqueeze(0).to(device))
+            image_vec_list.append(encoder(origin_image_list[i]))
+        # create the converted image
+        for img_index in range(len(image_vec_list)):
+            for cordinate in range(latent_size):
+                temp_vec = image_vec_list[img_index].detach().clone()
+                for step in range(num_of_steps + 1):
+                    # add the offset to the curent cordinate in the original vector
+                    temp_vec[0][cordinate] = -1 + step * (2 / num_of_steps)
+                    changed_image = decoder(temp_vec)
+                    # plot the decoder on the original vector
+                    if (step == 0):
+                        # the original image
+                        ax = plt.subplot(latent_size, num_of_image + 3, cordinate * (num_of_steps + 3) + 1)
+                        plt.imshow(origin_image_list[img_index].cpu().squeeze().numpy(), cmap='gist_gray')
+                        plt.title(f"Original image")
+                        # decoder on the original latent vector
+                        ax = plt.subplot(latent_size, num_of_image + 3, cordinate * (num_of_steps + 3) + 2)
+                        plt.imshow(decoder(image_vec_list[img_index]).cpu().squeeze().numpy(), cmap='gist_gray')
+                        plt.title(f"Original vector")
+                    # add to the converted image to the plot
+                    ax = plt.subplot(latent_size, num_of_image + 3, cordinate * (num_of_steps + 3) + step + 3)
+                    plt.imshow(changed_image.cpu().squeeze().numpy(), cmap='gist_gray')
+                    plt.title(f"cord: {cordinate}. value: {-1 + step * (2 / num_of_steps)}")
+                    ax.get_xaxis().set_visible(False)
+                    ax.get_yaxis().set_visible(False)
+                    # add to the the converted image to the changed list
+                    changed_image_list.append(changed_image)
+            plt.show()
